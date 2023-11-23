eb0626d553edfa1cf8b9e8fde8622ade33e12c62
lucas bilancini <lucasbilancini@gmail.com>
2023-11-23 18:39:43 +0100

    centering title show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :welcome]
 
   # Je peux voir tous les produits proposés par la plateforme SANS être connecté
   def home
