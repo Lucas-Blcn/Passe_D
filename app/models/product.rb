8c1573cd2ff7e2d8cfe1692a04e7cdd7f779cc79
lucas bilancini <lucasbilancini@gmail.com>
2023-11-24 15:27:36 +0100

    add depent destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many_attached :photos
 
   # validates :title, presence: true
